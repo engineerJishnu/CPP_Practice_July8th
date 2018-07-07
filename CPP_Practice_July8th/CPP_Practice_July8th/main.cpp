f837acebd73f5041590e03a813cefb3226be9574
Jishnu Chakrabarti <jishnuchakrabarti@Jishnus-MacBook-Air.local>
2018-07-07 13:12:41 +0400

    update

---
+++ CPP_Practice_July8th/CPP_Practice_July8th/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_Practice_July8th
+//
+//  Created by Jishnu Chakrabarti on 7/7/18.
+//  Copyright © 2018 Jishnu Chakrabarti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
